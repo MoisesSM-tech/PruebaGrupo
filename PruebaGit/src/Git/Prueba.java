4ff8052f6477774d74561ca593197c11a4a4a966
Fathi <Fathi@DESKTOP-4SM2BF2>
2025-03-27 16:45:39 +0100

    Este Commit lo ha creado Othmane Fathi

--- PruebaGit/src/Git/Prueba.java
+++ PruebaGit/src/Git/Prueba.java
@@ -5,6 +5,8 @@ public class Prueba {
 	public static void main(String[] args) {
 		//Este print lo ha creado Moises
 System.out.println("Este println lo ha creado Moises Saez");
+//Este print lo ha creado Fathi
+System.out.println("Este println lo ha creado Othmane Fathi");
 	}
 
 }
