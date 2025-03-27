20e114eed72d1a525dc592f4b69e25e8d8308b64
Moises <moisessaez2005@hotmail.com>
2025-03-27 16:24:40 +0100

    Primer commit para añadir todos los archivos necesario , por Moises

---
+++ PruebaGit/src/Git/Prueba.java
@@ -0,0 +1,10 @@
+package Git;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		//Este print lo ha creado Moises
+System.out.println("Este println lo ha creado Moises Saez");
+	}
+
+}
